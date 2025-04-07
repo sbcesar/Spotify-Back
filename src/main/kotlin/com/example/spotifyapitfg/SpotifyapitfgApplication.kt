f619e290149a135a2f4e019fd62ff6882a3b6cc5
sbcesar <148891359+sbcesar@users.noreply.github.com>
2025-04-07 20:30:11 +0200

    first commit :p

---
+++ src/main/kotlin/com/example/spotifyapitfg/SpotifyapitfgApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spotifyapitfg
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpotifyapitfgApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpotifyapitfgApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/spotifyapitfg/SpotifyapitfgApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.spotifyapitfg
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpotifyapitfgApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
